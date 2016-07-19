65a6628a950c06db9a0741988b7b81f2e344a26a
EdwardGoomba <edwardvdanilyuk@gmail.com>
2016-07-19 09:11:01 -0700

    Add twitter API info

--- include/twitter/tweets.php
+++ include/twitter/tweets.php
@@ -7,10 +7,10 @@ if( isset( $_GET['username'] ) AND $_GET['username'] != '' ):
 
 	$username = $_GET['username'];
 	$limit = ( isset( $_GET['count'] ) AND $_GET['count'] != '' ) ? $_GET['count'] : 2;
-	$consumerkey = "consumerkey";
-	$consumersecret = "consumersecret";
-	$accesstoken = "accesstoken";
-	$accesstokensecret = "accesstokensecret";
+	$consumerkey = "	WxsZptxGuvZFYZHI8NDX6zQQW";
+	$consumersecret = "ANuFsL3eZcq6rWG3fUI1hkO3CcV3iyoSwVuvNg9u73iwJC9Gik";
+	$accesstoken = "	3266470700-3DGLUPAYEE7o260rOtR8IWPJw9RwyiQCUmlSRGX";
+	$accesstokensecret = "ZNRzQ30ay8gtOa1q8gvCE182WWEvT0cNrGJYV2G4sFZhr";
 
 	function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
 	  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
